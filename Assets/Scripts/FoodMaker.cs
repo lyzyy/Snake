3161e62c3a13dcd61e3f76ac281cbe30f363876c
麻将1 <85428374@qq.com>
2019-05-24 10:21:07 +0800

    Initial commit

---
+++ Assets/Scripts/FoodMaker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodMaker : MonoBehaviour
+{
+    public GameObject foodPrefab;
+    public Sprite[] foodSprites;
+    //private float time;
+    private Transform foodHolder;
+    private int xMax=15;
+    private int yMax=12;
+    private int xMin=-15;
+    private int yMin=-11;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foodHolder = GameObject.Find("FoodHolder").transform;
+        //InvokeRepeating("MakeFood",0, time);
+        Resources.Load
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void MakeFood()
+    {
+        int index = Random.Range(0,foodSprites.Length);
+        GameObject food = Instantiate(foodPrefab);
+        food.transform.SetParent(foodHolder, false);
+        int x = Random.Range(xMin,xMax);
+        int y = Random.Range(yMin, yMax);
+        food.transform.localPosition = new Vector3(x*30,y*30,0);
+       
+    }
+}
---
+++ Assets/Scripts/SnakeHead.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeHead : MonoBehaviour
+{
+    public float velocity;//速度
+    public int step;//每次前进几步
+    private int x;
+    private int y;
+    private Vector3 headPos;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        InvokeRepeating("Move", 0, velocity);
+        x = 0;
+        y = step;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            x = 0;y = step;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            x = 0;y = -step;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            x = -step;y = 0;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            x = step;y = 0;
+        }
+    }
+    void Move()
+    {
+        headPos = gameObject.transform.localPosition;
+        gameObject.transform.localPosition = new Vector3(headPos.x + x,headPos.y + y,headPos.z);
+        
+    }
+}
